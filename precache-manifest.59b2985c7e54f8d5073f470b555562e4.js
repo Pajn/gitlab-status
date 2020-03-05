0216c1919f3938fd213e145c0bf2aeff7390c407
Rasmus Eneman <rasmus@eneman.eu>
2020-03-05 12:57:17 +0100

    Updates

---
+++ precache-manifest.59b2985c7e54f8d5073f470b555562e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bee9ec07bc5a873156b49b34305b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68922fdb5e9b81e23a47",
+    "url": "/static/css/main.7f8c9d2b.chunk.css"
+  },
+  {
+    "revision": "f8ab1bd1255060e7d112",
+    "url": "/static/js/2.6b1b79ef.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/static/js/2.6b1b79ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68922fdb5e9b81e23a47",
+    "url": "/static/js/main.60556055.chunk.js"
+  },
+  {
+    "revision": "ea8b6429e2fd620dbbf2",
+    "url": "/static/js/runtime-main.69ecb497.js"
+  },
+  {
+    "revision": "fb36fc3e0099a386dea954cd58750f13",
+    "url": "/static/media/failedIcon.fb36fc3e.svg"
+  },
+  {
+    "revision": "eeff3d0febf8ffbb85b6e6adf13d1b81",
+    "url": "/static/media/inProgressIcon.eeff3d0f.svg"
+  },
+  {
+    "revision": "eecad4abd3a849bd993b5833790396d2",
+    "url": "/static/media/successIcon.eecad4ab.svg"
+  }
+]);
